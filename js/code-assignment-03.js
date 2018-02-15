d5f55faee671afefef35f536c007976ba25aa11f
bweyen2 <bweyen2@users.noreply.github.com>
2018-02-15 10:42:26 -0600

    update

---
+++ js/code-assignment-03.js
@@ -0,0 +1,5 @@
+let projectId = 1
+let projecTitle = "Earthquake Map"
+let thumnail = true
+let  projecTitles = ["Earthquake Map", "Map of Earthquakes of Magnitude 2.5 or Greater"]
+console.log(projectId)
