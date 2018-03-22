d6be0eb4c97704a4e1166db0564fb7e4a12413db
bweyen2 <bweyen2@users.noreply.github.com>
2018-03-22 12:07:21 -0500

    update

--- map/webmap-assignment-01.js
+++ map/webmap-assignment-01.js
@@ -1,12 +1,22 @@
-let theworld = L.map('Brookemap').setView([29.953003, -90.071647], 13)
+let theworld = L.map('Brookemap').setView([29.951105,	-90.081227], 13)
 let basemap = 'https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}'
 L.tileLayer(basemap).addTo(theworld)
-let mypoint = L.marker([29.953003, -90.071647]).addTo(theworld)
-let polygon = L.polygon([
+let mypoint = L.marker([29.951105,	-90.081227]).addTo(theworld)
+let myshape = L.polygon([
   [29.951848,	-90.088608],
   [29.959582,	-90.075734],
   [29.944449,	-90.076935]
-]).addTo(theworld);
+]).addTo(theworld)
 
-myshape.bindPopup('St. Katharine\'s and Wapping')
-mypoint.bindPopup('London Bridge Station')
+myshape.bindPopup('New Orleans')
+mypoint.bindPopup('Mercedes-Benz Superdome')
+
+theworld.on('click', function (event) {
+  console.log('You clicked the map at ' + event.latlng)
+})
+var latlngs = [
+    [29.951127,	-90.081347],
+    [29.948412,	-90.070361],
+    [29.949565,	-90.062936]
+];
+var polyline = L.polyline(latlngs, {color: 'red'}).addTo(theworld);
