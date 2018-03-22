da7008bc16620aa3d6080b59c9e84104e9640137
bweyen2 <bweyen2@users.noreply.github.com>
2018-03-22 11:39:41 -0500

    update

--- map/webmap-assignment-01.js
+++ map/webmap-assignment-01.js
@@ -1,4 +1,4 @@
 //let someName = L.map('your-map-div-id').setView([yourLat, yourLon], yourZoom)
 let theworld = L.map('Brookemap').setView([29.953003, -90.071647], 13)
-let basemap = 'https://{s}.{base}.maps.cit.api.here.com/maptile/2.1/{type}/{mapID}/normal.day/{z}/{x}/{y}/{size}/{format}?app_id={app_id}&app_code={app_code}&lg={language}'
+let basemap = 'https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}'
 L.tileLayer(basemap).addTo(theworld)
