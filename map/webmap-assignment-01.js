edfc4dde287c147835cfa8d5a1a8883763eac845
bweyen2 <bweyen2@users.noreply.github.com>
2018-03-22 11:47:35 -0500

    ok

--- map/webmap-assignment-01.js
+++ map/webmap-assignment-01.js
@@ -1,4 +1,12 @@
-//let someName = L.map('your-map-div-id').setView([yourLat, yourLon], yourZoom)
 let theworld = L.map('Brookemap').setView([29.953003, -90.071647], 13)
 let basemap = 'https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}'
 L.tileLayer(basemap).addTo(theworld)
+let mypoint = L.marker([29.953003, -90.071647]).addTo(theworld)
+let polygon = L.polygon([
+  [29.951848,	-90.088608],
+  [29.959582,	-90.075734],
+  [29.944449,	-90.076935]
+]).addTo(theworld);
+
+myshape.bindPopup('St. Katharine\'s and Wapping')
+mypoint.bindPopup('London Bridge Station')
