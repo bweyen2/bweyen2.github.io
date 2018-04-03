07448aeaf7a7f9c9369218adaf779624719ff754
bweyen2 <bweyen2@users.noreply.github.com>
2018-04-03 11:42:11 -0500

    update

--- map/webmap-assignment-03.js
+++ map/webmap-assignment-03.js
@@ -1,6 +1,6 @@
 let theworld = L.map('Brookemap').setView([29.951105,	-90.081227], 13)
-let basemap = 'https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}'
-L.tileLayer(basemap).addTo(theworld)
+
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}').addTo(theworld)
 
 let myredpin = L.icon({
   iconUrl: 'redpin.png',
@@ -9,12 +9,16 @@ let myredpin = L.icon({
   popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
 })
 
-let markerCoords = [51.5, -0.09]
-let markerOptions = { icon: myredpin }
+let myMarker = L.marker([29.951105,	-90.081227], {icon: myredpin}).addTo(theworld);
 
-let mypoint = L.marker([29.951105,	-90.081227]).addTo(theworld)
-let myshape = L.polygon([
+let mypolystyle = {
+  color: 'orange',
+  fillColor: 'yellow'
+}
+
+let mypolycoords = [
   [29.951848,	-90.088608],
   [29.959582,	-90.075734],
   [29.944449,	-90.076935]
-]).addTo(theworld)
+
+let myPolygon = L.polygon(mypolycoords, mypolystyle).addTo(theworld)
