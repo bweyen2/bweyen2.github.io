957e9cd28fb36811cc7b95c2489b9f3f76ab574d
bweyen2 <bweyen2@users.noreply.github.com>
2018-04-05 11:49:15 -0500

    update

---
+++ map/webmap-assignment-04.js
@@ -0,0 +1,27 @@
+let theworld = L.map('Brookemap').setView([38,	-98], 4)
+let basemap = 'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}'
+L.tileLayer(basemap).addTo(theworld)
+
+myGeojsonstyle = function (state) {
+  let age = feature.properties.MED_AGE
+   let stateColor = 'Olive'
+   if ( age < 38 ) { stateColor = 'Green' }
+   let myStyle = {
+     color: stateColor,
+     weight: 1,
+     fillOpacity: 0.2
+   }
+   return myStyle
+}
+
+function createPopup (state, statelayer) {
+  let name = feature.properties.STATE_NAME
+  let age = feature.properties.MED_AGE
+  statelayer.bindPopup('Median age of ' + name + ': ' + age + '<br>National average: 38')
+}
+
+myGeojsonOpOptions = {
+  style: myGeojsonstyle,
+  onEachFeature: createPopup
+}
+L.geoJSON(stateDemographics, myGeojsonOpOptions).addTo(theworld)
