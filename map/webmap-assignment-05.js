91a3e2f581603a6758ff0e6520bc8881014bbeef
bweyen2 <bweyen2@users.noreply.github.com>
2018-04-19 10:53:52 -0500

    k

--- map/webmap-assignment-05.js
+++ map/webmap-assignment-05.js
@@ -14,7 +14,7 @@ let worldbasemap = {
 
 geoJSONstyle = function (states) {
   let age = states.properties.MED_AGE
-  let statecolor = 'Blue'
+  let statecolor = 'Light Blue'
   if ( age < 38 ) {statecolor = 'Pink'}
   let mystyle = {
     color: statecolor,
